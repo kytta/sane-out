2cb7ebdaa191da1f7d087b0a591b7573b508b922
Nikita Karamov <nick@karamoff.dev>
2021-03-03 22:29:51 +0100

    Remove old unittests
    For now, doctests are used. Later I will migrate to pytest

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-from unittest.main import main
-
-
--- tests/test_sane_out.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from sane_out import __version__
-
-
-class TestSaneOut(TestCase):
-    def test_version(self):
-        self.assertEqual(__version__, "0.0.1")
