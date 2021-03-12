1e5ec0d5542abdf00364f3f7a405740b85f88235
Nikita Karamov <nick@karamoff.dev>
2021-03-12 22:29:13 +0100

    v0.2.0

--- sane_out/__init__.py
+++ sane_out/__init__.py
@@ -1,10 +1,10 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 import sys
 
 from sane_out.printer import _SanePrinter
 
-if sys.platform == 'win32':
+if sys.platform == "win32":
     import colorama
 
     colorama.init()
